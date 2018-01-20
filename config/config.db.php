50efbc76519f5711529f61ad387293c282e1de28
Loi Tran <loib1505835@student.ctu.edu.vn>
2018-01-20 14:54:35 +0700

    Update config.db.php, README.md

--- config/config.db.php
+++ config/config.db.php
@@ -1,4 +1,9 @@
 <?php
-
-
+    $conn = new mysqli("localhost", "root", "", "BookLibraryManagement");
+    
+    /* check connection */
+    if ($conn->connect_errno) {
+        printf("Connect failed: %s\n", $conn->connect_error);
+        exit();
+    }
 ?>
