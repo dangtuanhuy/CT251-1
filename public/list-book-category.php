d1250aba9af2884e7c95fc23b40e4214401ade5a
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-04-01 22:09:08 +0700

    Sửa danh mục Category

--- public/list-book-category.php
+++ public/list-book-category.php
@@ -14,21 +14,50 @@ include ROOT_PATH.'/config/config.db.php';
             <div class="col-md-4">
                 <ul class="list-group">
                     <li class="list-group-item active "><p class="text-center">Book Category 1<p></li>
-                    <li class="list-group-item">Dapibus ac facilisis in</li>
-                  
+                    <?php 
+							
+							$result = mysqli_query($conn,"SELECT `CategoryNames` FROM `category` LIMIT 0, 8");
+							while($row=mysqli_fetch_array($result, MYSQLI_ASSOC))
+							{
+								?>
+                    <li class="list-group-item"><a href="#"><?php echo $row["CategoryNames"] ?></a></li>
+                    <?php
+									
+								}
+								?>
                 </ul>
             </div>
             <div class="col-md-4">
                 <ul class="list-group">
                 <li class="list-group-item active"><p class="text-center">Book Category 2<p></li>
-                    <li class="list-group-item">Dapibus ac facilisis in</li>
+                <?php 
+							
+							$result = mysqli_query($conn,"SELECT `CategoryNames` FROM `category` LIMIT 8,8 ");
+							while($row=mysqli_fetch_array($result, MYSQLI_ASSOC))
+							{
+								?>
+                    <li class="list-group-item"><a href="#"><?php echo $row["CategoryNames"] ?></a></li>
+                    <?php
+									
+								}
+								?>
                     
                 </ul>
             </div>
             <div class="col-md-4">
                 <ul class="list-group">
                 <li class="list-group-item active"><p class="text-center">Book Category 3<p></li>
-                    <li class="list-group-item">Dapibus ac facilisis in</li>
+                <?php 
+							
+							$result = mysqli_query($conn,"SELECT `CategoryNames` FROM `category` LIMIT 16,8 ");
+							while($row=mysqli_fetch_array($result, MYSQLI_ASSOC))
+							{
+								?>
+                    <li class="list-group-item"><a href="#"><?php echo $row["CategoryNames"] ?></a></li>
+                    <?php
+									
+								}
+								?>
                     
                 </ul>
             </div>
