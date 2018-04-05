1fc6365508f545212f7c64856e7935f48e6bec91
Loi Tran <loib1505835@student.ctu.edu.vn>
2018-04-06 03:40:56 +0700

    Fixed nav responsive

--- templates/_footer.php
+++ templates/_footer.php
@@ -4,5 +4,13 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src=<?= PUBLIC_PATH."/js/bootstrap.min.js"?>></script>
+    <script>
+    $(document).ready(function(){
+        const width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+        if (width <= 430) {
+            $("#horizontal-nav>a").removeClass("btn-lg");
+        }
+    })
+    </script>
 </body>
 </html>
