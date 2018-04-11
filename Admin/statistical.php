cee0ba18ac398bc5acc94cf843854fc30cf9a964
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-04-11 11:58:32 +0700

    Count

--- Admin/statistical.php
+++ Admin/statistical.php
@@ -1,88 +1,5 @@
  <!-- Counts Section -->
-<<<<<<< HEAD
 
-        <div class="container-fluid">
-          <div class="row">
-            <!-- Count item widget-->
-            <div class="col-xl-2 col-md-4 col-6">
-              <div class="wrapper count-title d-flex">
-                <div class="icon"><i class="icon-page"></i></div>
-                <div class="name"><strong class="text-uppercase">Category</strong><span>Total</span>
-                <?php 
-                $result = mysqli_query($conn,"SELECT * FROM `category` ");
-                $num_rows = mysqli_num_rows($result);
-                ?>
-                  <div class="count-number"><?php echo $num_rows ?></div>
-                </div>
-              </div>
-            </div>
-            <!-- Count item widget-->
-            <div class="col-xl-2 col-md-4 col-6">
-              <div class="wrapper count-title d-flex">
-                <div class="icon"><i class="icon-padnote"></i></div>
-                <div class="name"><strong class="text-uppercase">Book</strong><span>Total</span>
-                <?php 
-                $result1 = mysqli_query($conn,"SELECT * FROM `book` ");
-                $num_rows1 = mysqli_num_rows($result1);
-                ?>
-                  <div class="count-number"><?php echo $num_rows1 ?></div>
-                </div>
-              </div>
-            </div>
-            <!-- Count item widget-->
-            <div class="col-xl-2 col-md-4 col-6">
-              <div class="wrapper count-title d-flex">
-                <div class="icon"><i class="icon-user"></i></div>
-                <div class="name"><strong class="text-uppercase">Custommer</strong><span>Total</span>
-                <?php 
-                $result2 = mysqli_query($conn," SELECT * FROM `user` WHERE `Role`=1 ");
-                $num_rows2 = mysqli_num_rows($result2);
-                ?>
-                  <div class="count-number"><?php echo $num_rows2 ?></div>
-                </div>
-              </div>
-            </div>
-            <!-- Count item widget-->
-            <div class="col-xl-2 col-md-4 col-6">
-              <div class="wrapper count-title d-flex">
-                <div class="icon"><i class="icon-bill"></i></div>
-                <div class="name"><strong class="text-uppercase">Feedback</strong><span>Not Views</span>
-                <?php
-                $result3 = mysqli_query($conn,"SELECT `FeedBackId`, `FeedBackTitle`, `FeedBackDate`, `FeedBackContent`, `FeedBackStatus` FROM `feedback` WHERE `FeedBackStatus`=0");
-                $num_rows3 = mysqli_num_rows($result3);
-                ?>
-                  <div class="count-number"><?php echo $num_rows3 ?></div>
-                </div>
-              </div>
-            </div>
-            <!-- Count item widget-->
-            <div class="col-xl-2 col-md-4 col-6">
-              <div class="wrapper count-title d-flex">
-                <div class="icon"><i class="icon-list"></i></div>
-                <div class="name"><strong class="text-uppercase">Lent</strong><span>Total</span>
-                <?php
-                $result4 = mysqli_query($conn,"SELECT * FROM `lenditem` WHERE `LendStatus`=1");
-                $num_rows4 = mysqli_num_rows($result4);
-                ?>
-                  <div class="count-number"><?php echo $num_rows4 ?></div>
-                </div>
-              </div>
-            </div>
-            <!-- Count item widget-->
-            <div class="col-xl-2 col-md-4 col-6">
-              <div class="wrapper count-title d-flex">
-                <div class="icon"><i class="icon-mail"></i></div>
-                <div class="name"><strong class="text-uppercase">Mail</strong><span>Not Active</span>
-                
-                <?php
-                $result5 = mysqli_query($conn,"SELECT * FROM `user` WHERE `Active`=0");
-                $num_rows5 = mysqli_num_rows($result5);
-                ?>
-                  <div class="count-number"><?php echo $num_rows5 ?></div>
-                </div>
-              </div>
-            </div>
-=======
 <section class="dashboard-header section-padding">
   <div class="container-fluid">
     <div class="row">
@@ -92,7 +9,7 @@
           <div class="icon"><i class="icon-user"></i></div>
           <div class="name"><strong class="text-uppercase">New Clients</strong><span>Last 7 days</span>
             <div class="count-number">25</div>
->>>>>>> master
+
           </div>
         </div>
       </div>
